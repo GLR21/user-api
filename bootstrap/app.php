a499e90b19b6f7ef68c4b268aa7a07d21fae1931
Gabriel Lange Ramos <gabriellange.ramos@gmail.com>
2024-04-22 20:29:42 -0300

    adding middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
         api: __DIR__.'/../routes/api.php',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->append([
+            \App\Http\Middleware\BearerAuthMiddleware::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
